48d646435a508631765d726caa9727429fc12045
Ed Porras <dev@digressed.net>
2026-07-26 11:34:21 +0200

    build: switch javac to use --release instead of -source/-target
    resolves "warning: [options] system modules path not set in conjunction with -source 11"

--- project.clj
+++ project.clj
@@ -35,7 +35,7 @@
   :target-path "target/%s/"
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
-  :javac-options ["-target" "11" "-source" "11" "-Xlint:unchecked"]
+  :javac-options ["--release" "11" "-Xlint:unchecked"]
   :deploy-repositories {"releases" {:url "https://repo.clojars.org"}}
   :profiles {:dev {:dependencies
                    [[com.oscaro/tools-io "0.3.43"]
